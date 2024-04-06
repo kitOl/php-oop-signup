110c146d9a73321a3479d8590ac69459962b93d1
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-06 20:45:11 +0300

    fixed some decorations

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ session_start();
       </div>
       <div class="index-login-login">
         <h4>LOGIN</h4>
-        <p>Have you already registered? Login here!</p>
+        <p>Already registered? Login here!</p>
         <form action="includes/login.inc.php" method="post">
           <input type="text" name="uid" placeholder="Username">
           <input type="password" name="pwd" placeholder="Password">
